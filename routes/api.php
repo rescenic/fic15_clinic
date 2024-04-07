7d7648d84ea1b7a1e18d7722a96850bb22947406
bahri <saiful.bahri.tl@gmail.com>
2024-04-07 12:47:29 +0700

    integartion satu sehat, get token

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Api\PatientController;
 use App\Http\Controllers\Api\DoctorScheduleController;
 use App\Http\Controllers\Api\ServiceMedicinesController;
 use App\Http\Controllers\Api\PatientScheduleController;
+use App\Http\Controllers\Api\SatuSehatTokenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,3 +46,6 @@ Route::apiResource('/api-service-medicines', ServiceMedicinesController::class)-
 
 //patient schedules
 Route::apiResource('/api-patient-schedules', PatientScheduleController::class)->middleware('auth:sanctum');
+
+//token
+Route::get('/satusehat-token', [SatuSehatTokenController::class, 'token']);
